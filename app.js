c10e6e6b70bedf8ae1cfad1362559a82c0af0709
Tumininu Sosanya <tumininu.sosanya@gmail.com>
2020-08-13 16:56:01 -0700

    Add score board to keep track of three rounds of play

--- app.js
+++ app.js
@@ -4,12 +4,33 @@ const dice2 = document.querySelectorAll("#dice")[1];
 const player = document.getElementById("player");
 const play = document.getElementById("play");
 const round = document.querySelectorAll(".list-group-item");
+const rounds = document.getElementById("rounds");
+const playAgain = document.getElementById("playAgain");
 
 let roundChecker = 0;
+let nengiWins = 0;
+let ozoWins = 0;
+
+function resetAll() {
+  dice1.setAttribute("src", "images/dice6.png");
+  dice2.setAttribute("src", "images/dice6.png");
+
+  roundChecker = 0;
+  nengiWins = 0;
+  ozoWins = 0;
+
+  round[0].innerHTML = "Round I : ";
+  round[1].innerHTML = "Round II : ";
+  round[2].innerHTML = "Round III : ";
+  round[3].innerHTML = "<strong>Winner</strong> : ";
+
+  rounds.innerHTML = "⌒(｡･.･｡)⌒ Round I";
+  play.removeAttribute("data-toggle");
+  play.removeAttribute("data-target");
+}
 
 play.addEventListener("click", function () {
   let randomNo1 = Math.floor(Math.random() * 6 + 1);
-  console.log(randomNo1);
   let randomDiceImage = `dice${randomNo1}.png`;
   let randomImageSource = `images/${randomDiceImage}`;
   dice1.setAttribute("src", randomImageSource);
@@ -19,45 +40,64 @@ play.addEventListener("click", function () {
   let randomDiceSource = `images/${randomDice}`;
   dice2.setAttribute("src", randomDiceSource);
 
-  // if (roundChecker === 0) {
-  //   round[0].innerHTML =
-  //     randomNo1 === randomNo2
-  //       ? "Round I : draw"
-  //       : randomNo1 > randomNo2
-  //       ? "Round I : Nengi"
-  //       : "Round I : Ozo";
-
-  //   roundChecker += 1;
-  // }
-
-  // if (roundChecker === 1) {
-  //   round[1].innerHTML =
-  //     randomNo1 === randomNo2
-  //       ? "Round II : draw"
-  //       : randomNo1 > randomNo2
-  //       ? "Round II : Nengi"
-  //       : "Round II : Ozo";
-
-  //   roundChecker += 1;
-  // }
-
-  // if (roundChecker === 2) {
-  //   round[2].innerHTML =
-  //     randomNo1 === randomNo2
-  //       ? "Round III : draw"
-  //       : randomNo1 > randomNo2
-  //       ? "Round III : Nengi"
-  //       : "Round III : Ozo";
-
-  //   roundChecker += 1;
-  // }
-
-
-
-  player.innerHTML =
-    randomNo1 === randomNo2
-      ? "It's a draw!"
+  function checkWinner(round) {
+    return randomNo1 === randomNo2
+      ? `${round} : <strong>Draw</strong>`
       : randomNo1 > randomNo2
-      ? "🏳 Nengi wins!"
-      : "🏳 Ozo wins!";
+      ? `${round}  : <strong>Nengi</strong>`
+      : `${round}  : <strong>Ozo</strong>`;
+  }
+
+  function winnerCount() {
+    if (randomNo1 > randomNo2) {
+      nengiWins += 1;
+    } else if (randomNo2 > randomNo1) {
+      ozoWins += 1;
+    }
+  }
+
+  if (roundChecker === 0) {
+    round[0].innerHTML = checkWinner("Round I");
+    roundChecker += 1;
+    winnerCount();
+  } else if (roundChecker === 1) {
+    rounds.innerHTML = "⌒(｡･.･｡)⌒ Round II";
+    round[1].innerHTML = checkWinner("Round II");
+
+    roundChecker += 1;
+
+    winnerCount();
+  } else if (roundChecker === 2) {
+    rounds.innerHTML = "⌒(｡･.･｡)⌒ Round III";
+    round[2].innerHTML = checkWinner("Round III");
+
+    roundChecker += 1;
+
+    winnerCount();
+  } else if (roundChecker === 3) {
+    round[3].innerHTML =
+      ozoWins === nengiWins
+        ? "<strong>Winner : DRAW</strong>"
+        : ozoWins > nengiWins
+        ? "<strong>Winner : 🏁 OZO</strong>"
+        : "<strong>Winner : 🏁 NENGI</strong>";
+
+    play.setAttribute("data-toggle", "modal");
+    play.setAttribute("data-target", "#myModal");
+  }
+
+  // player.innerHTML =
+  //   randomNo1 === randomNo2
+  //     ? "It's a draw!"
+  //     : randomNo1 > randomNo2
+  //     ? "🏳 Nengi wins!"
+  //     : "🏳 Ozo wins!";
+});
+
+reset.addEventListener("click", () => {
+  resetAll();
+});
+
+playAgain.addEventListener("click", () => {
+  resetAll();
 });
